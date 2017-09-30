c73a80d394ee9715c82b452d1cd59ce818c75fce
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2017-09-30 20:15:38 +0700

    First Commit

---
+++ balikKalimat.js
@@ -0,0 +1,10 @@
+function balikKalimat(kalimat) {
+  return kalimat.split('').reverse().join('');
+
+}
+
+console.log(balikKalimat('Super Hero'));
+console.log(balikKalimat('John Doe'));
+console.log(balikKalimat('I am a bookworm'));
+console.log(balikKalimat('Coding is my hobby'));
+console.log(balikKalimat('Hungry'));
